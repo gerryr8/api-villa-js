c98f1979c4718195679768022c53089a2b905a09
Josue omar catzin chacon <20393135@utcancun.edu.mx>
2023-07-21 13:43:43 -0500

    Update

--- API/single product/productdeteail.js
+++ API/single product/productdeteail.js
@@ -1,84 +1,31 @@
-// Función para cargar los detalles del producto
-function loadProductDetails(productId) {
-    fetch(`https://fakestoreapi.com/products/${productId}`)
-        .then(res => res.json())
-        .then(product => {
-            const productImage = document.getElementById("product-image");
-            const productTitle = document.getElementById("product-title");
-            const productDescription = document.getElementById("product-description");
-            const productPrice = document.getElementById("product-price");
-            const productCategory = document.getElementById("product-category");
-            const productRate = document.getElementById("product-rate");
+  // Obtener el ID del producto de la URL
+  const urlParams = new URLSearchParams(window.location.search);
+  const productId = urlParams.get("id");
 
-            productImage.src = product.image;
-            productImage.alt = product.title;
-            productTitle.textContent = product.title;
-            productDescription.textContent = product.description;
-            productPrice.textContent = `$${product.price}`;
-            productCategory.textContent = `Category: ${product.category}`;
-            productRate.textContent = `Category: ${product.rating.rate}`;
+  // Función para cargar los detalles del producto
+  function loadProductDetails(productId) {
+      fetch(`https://fakestoreapi.com/products/${productId}`)
+          .then(res => res.json())
+          .then(product => {
+              const productImage = document.getElementById("product-image");
+              const productTitle = document.getElementById("product-title");
+              const productDescription = document.getElementById("product-description");
+              const productPrice = document.getElementById("product-price");
+              const productCategory = document.getElementById("product-category");
+              const productRate = document.getElementById("product-rate");
 
-            // Guardar el objeto 'product' en una variable global para usarlo en la función de edición
-            window.currentProduct = product;
+              productImage.src = product.image;
+              productImage.alt = product.title;
+              productTitle.textContent = product.title;
+              productDescription.textContent = product.description;
+              productPrice.textContent = `$${product.price}`;
+              productCategory.textContent = `Category: ${product.category}`;
+              productRate.textContent = `Category: ${product.rating.rate}`;
 
-            // Agregar evento al botón para editar
-            const editButton = document.getElementById("edit-button");
-            editButton.addEventListener("click", editProduct);
-        })
-        .catch(error => {
-            console.error(error);
-        });
-}
+          })
+          .catch(error => {
+              console.error(error);
+          });
+  }
 
-// Función para editar el producto
-function editProduct() {
-    // Verificar si hay un producto seleccionado
-    if (!window.currentProduct) {
-        console.error("No hay producto seleccionado.");
-        return;
-    }
-
-    // Crear el objeto con los datos actualizados del producto
-    const updatedProduct = {
-        title: 'test product',
-        price: 13.5,
-        description: 'lorem ipsum set',
-        image: 'https://i.pravatar.cc',
-        category: 'electronic'
-    };
-
-    // Realizar la solicitud PUT para editar el producto
-    fetch(`https://fakestoreapi.com/products/${window.currentProduct.id}`, {
-        method: "PUT",
-        body: JSON.stringify(updatedProduct),
-        headers: {
-            "Content-Type": "application/json"
-        }
-    })
-    .then(res => res.json())
-    .then(updatedProductData => {
-        // Aquí obtendrás los datos actualizados del producto desde la API
-        console.log("Producto editado:", updatedProductData);
-
-        // Actualizar los detalles del producto en la página con los nuevos datos si es necesario
-        const productTitle = document.getElementById("product-title");
-        const productDescription = document.getElementById("product-description");
-        const productPrice = document.getElementById("product-price");
-
-        productTitle.textContent = updatedProductData.title;
-        productDescription.textContent = updatedProductData.description;
-        productPrice.textContent = `$${updatedProductData.price}`;
-
-        // Actualizar el objeto 'currentProduct' con los nuevos datos
-        window.currentProduct.title = updatedProductData.title;
-        window.currentProduct.description = updatedProductData.description;
-        window.currentProduct.price = updatedProductData.price;
-    })
-    .catch(error => {
-        console.error("Error al editar el producto:", error);
-    });
-}
-
-// Llamar a la función loadProductDetails con el ID del producto seleccionado (reemplaza 7 con el ID del producto que deseas cargar)
-const productId = 7;
-loadProductDetails(productId);
+  loadProductDetails(productId);
--- API/single product/single-product.js
+++ API/single product/single-product.js
@@ -1,62 +1,39 @@
+const url = "https://fakestoreapi.com/products";
 
-// const url = "https://fakestoreapi.com/products";
+    function redirectToProductDetails(productId) {
+        window.location.href = `productDetails.html?id=${productId}`;
+    }
 
-// fetch(url)
-//   .then(res => res.json())
-//   .then(data => {
-//     const productCard= document.getElementById("product-content");
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            const productContainer = document.getElementById("product-container");
 
-//     data.forEach(product => {
-//       const productContent = document.createElement("div");
-//         productContent.innerHTML = `
-//         <h2 class="produt-title">${product.title}</h2>
-//         <p class="description">${product.description}</p>
-//         <img class="product-img" src="${product.image}" alt="${product.title}" />
-//       `;
-//       productCard.appendChild(productContent);
-//     });
-//   })
-//   .catch(error => {
-//     console.error( error);
-//   });
+            data.forEach(product => {
+                const productCard = document.createElement("div");
+                productCard.classList.add("product-card");
 
+                const productContent = document.createElement("div");
+                productContent.classList.add("product-content");
+                productContent.innerHTML = `
+                    <div class="img-container"> 
+                        <img class="product-img" src="${product.image}" alt="${product.title}" />
+                    </div>
+                    <h2 class="product-title">${product.title}</h2>
+                    <div class="description-over">
+                        <p class="description">${product.category}</p>
+                    </div>
+                    <div class="price-container">
+                        <p class="price">$${product.price}</p>
+                    </div>
+                    <div class="pay-container"> 
+                        <button class="pay" onclick="redirectToProductDetails(${product.id})">Detalles</button>
+                    </div>`;
 
-  const url = "https://fakestoreapi.com/products";
-
-fetch(url)
-  .then(res => res.json())
-  .then(data => {
-    const productContainer = document.getElementById("product-container");
-
-    data.forEach(product => {
-      const productCard = document.createElement("div");
-      productCard.classList.add("product-card");
-
-      const productContent = document.createElement("div");
-      productContent.classList.add("product-content");
-      productContent.innerHTML = ` 
-      <div class="img-container"> 
-      <img class="product-img" src="${product.image}" alt="${product.title}" />
-
-    </div>
- 
-      <h2 class="product-title">${product.title}</h2>
-      <div class="description-over">
-      <p class="description">${product.category}</p>
-      </div>
-      <div class="price-container">
-      <p class="price">$${product.price}</p>
-      </div>
-      <div class="pay-container"> 
-        <button class="pay">Comprar</button>
-      </div>
-       
-    
-        `;
-      productCard.appendChild(productContent);
-      productContainer.appendChild(productCard);
-    });
-  })
-  .catch(error => {
-    console.error(error);
-  });
+                productCard.appendChild(productContent);
+                productContainer.appendChild(productCard);
+            });
+        })
+        .catch(error => {
+            console.error(error);
+        });
